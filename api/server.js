e305fb232583fbc554f1d2127ec6e8b0743058ae
Heather Beckman <heatherbeckman85@gmail.com>
2020-12-16 22:34:06 -0700

    wrote accounts api with CRUD and tested in Insomnia

--- api/server.js
+++ api/server.js
@@ -6,4 +6,70 @@ const server = express();
 
 server.use(express.json());
 
+server.get("/accounts", async (req, res, next) => {
+    try {
+        const accounts = await db.select("*").from("accounts")
+        res.json(accounts)
+    } catch (err) {
+        next(err)
+    }
+})
+
+server.get("/accounts/:id", async (req, res, next) => {
+    try {
+        const account = await db.select("*")
+                            .from("accounts")
+                            .where("id", req.params.id)
+                            .limit(1)
+        res.json(account)
+    } catch (err) {
+        next(err)
+    }
+})
+
+server.post("/accounts", async (req, res, next) => {
+    try {
+        const payload = {
+            name: req.body.name,
+            budget: req.body.budget
+        }
+
+        const [id] = await db.insert(payload)
+                        .into("accounts")
+        const account = await db("accounts")
+                            .first()
+                            .where("id", id)
+        res.status(201).json(account)
+    } catch (err) {
+        next(err)
+    }
+})
+
+server.put("/accounts/:id", async (req, res, next) => {
+    try {
+        const payload = {
+            name: req.body.name,
+            budget: req.body.budget
+        }
+        await db("accounts")
+            .where("id", req.params.id)
+            .update(payload)
+
+        const account = await db("accounts").where("id", req.params.id).first()
+        res.json(account)
+
+    } catch (err) {
+        next(err)
+    }
+})
+
+server.delete("/accounts/:id", async (req, res, next) => {
+    try {
+        await db("accounts").where("id", req.params.id).del()
+        res.status(204).end()
+    } catch (err) {
+        next(err)
+    }
+})
+
 module.exports = server;
